6f8115f76d1f129ac34d2208e36f0940aa35e4ff
David Banks <davidlbanks713@yahoo.com>
2016-10-26 15:45:32 -0700

    added code to repo

---
+++ timezone_drill.py
@@ -0,0 +1,34 @@
+import datetime
+from datetime import time
+
+potime= datetime.datetime.now()
+
+print "Current Time in Portland:"
+print potime
+
+
+#NY time
+nytime = potime + datetime.timedelta(hours= 3, minutes= 00)
+ytime = time(hour=nytime.hour)
+print "Current Time in New York:"
+print ytime
+
+if ytime > datetime.time(hour= 9, minute = 0) and  ytime < datetime.time(hour= 21, minute = 0):
+    print " The New York Branch Is Currently Open. "
+
+else:
+    print " The New York Branch Is Currently Closed. "
+
+#London Time
+lotime = potime + datetime.timedelta(hours= 8, minutes= 00)
+ltime = time(hour= lotime.hour)
+print "Current Time in London:"
+print ltime
+
+if ltime > datetime.time(hour=9, minute= 00) and ltime < datetime.time(hour=21, minute= 00):
+    print " The London Branch Is Currently Open. "
+
+else:
+    print " The London Branch Is Currently Closed. "
+
+
